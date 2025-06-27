6e9e3dc896252e53ae6a214a633a0615c6bf0de7
Anshika-Si <singha23@iitk.ac.in>
2025-06-28 03:53:58 +0530

    Add files via upload

---
+++ ACA_Assignment1/java.js
@@ -0,0 +1,4 @@
+
+let box=document.querySelector('#box');
+box.addEventListener('mouseenter',()=>box.style.backgroundColor='purple')
+box.addEventListener('mouseleave',()=>box.style.backgroundColor='coral')
